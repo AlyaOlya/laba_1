bcc9e39c6f2979dc025e78d645e1dac1c35ca0a8
AlyaOlya <125035100+AlyaOlya@users.noreply.github.com>
2023-02-17 16:52:51 +0300

    Delete 10 directory

--- 10/zadanie1.py
+++
@@ -1,8 +0,0 @@
-password =  input ('Введите пароль : ')
-if len(password) < 7:
-    print ("Пароль слишком короткий. ")
-elif password[0:8] == "1234567":
-    print('Пароль ненадёжный! ')
-else:
-    print("Пароль подходит. ")
-
